c21b9771abdb1e5325884ea36ef31c96ca974291
mabiiak <mabiane.polniak@gmail.com>
2023-04-17 23:03:49 -0300

    feat: Cria estado para salvar habilidades e cria componente base Proeficiencia

---
+++ src/components/Proeficiencia.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+function Proeficiencia() {
+    const [listProeficiencia, setListaProeficiencia] = useState([]);
+
+    return (
+        <div>
+            <input type="text" />
+            <p>0</p>
+            <button>+</button>
+        </div>
+    )
+}
+
+export default Proeficiencia;
--- src/paginas/Ficha.jsx
+++ src/paginas/Ficha.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Habilidades from "../components/Habilidades";
+import Proeficiencia from "../components/Proeficiencia";
 import TituloExpansivo from "../components/genericos/TituloExpansivo";
 
 export default function Ficha() {
-    const habilidades = () => (<Habilidades />);
+    const [defineHabilidades, setHabilidades] = useState({
+        forca: 0,
+        destreza: 0,
+        constituicao: 0,
+        inteligencia: 0,
+        sabedoria: 0,
+        carisma: 0,
+    });
+
+    const habilidadesProeficiencias = () => (
+        <div>
+            <Habilidades
+                defineHabilidades={defineHabilidades}
+                setHabilidades={setHabilidades}
+            />
+            {/* <Proeficiencia /> */}
+        </div>
+    );
 
     return(
         <div>
@@ -15,7 +33,7 @@ export default function Ficha() {
 
             <TituloExpansivo
                 titulo="Habilidades e Proeficiencias"
-                body={habilidades()}
+                body={habilidadesProeficiencias()}
             />
 
             {/* Magias */}
