5f2b68b193476b4c579166f0a8010336a7945adb
mabiiak <mabiane.polniak@gmail.com>
2023-07-02 11:39:01 -0300

    feat: adiciona animação ao expandir campo

--- src/components/genericos/TituloExpansivo.jsx
+++ src/components/genericos/TituloExpansivo.jsx
@@ -1,19 +1,44 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import '../css/Titulos.css'
 
 function TituloExpansivo({ titulo, body }) {
-  const [viewHidden, setViewHidden] = useState(true)
+  const [expanded, setExpanded] = useState(false);
+  const [divHeight, setDivHeight] = useState(0);
 
   const openContent = () => {
-    setViewHidden(!viewHidden)
-  }
+    setExpanded(!expanded);
+    setDivHeight(expanded ? 0 : divRef.current.scrollHeight);
+  };
+
+  const divRef = useRef(null);
+
+  const handleTransitionEnd = () => {
+      if (expanded) {
+        setDivHeight(divRef.current.scrollHeight);
+      } else {
+        setDivHeight(0);
+      }
+    };
 
   return (
-    <section className="geral">
-      <div id="titulos" onClick={() => openContent()}>
-        <p> {titulo} </p>
+    <section
+      ref={divRef}
+      className={`geral expandable ${expanded ? "expanded" : ""}`}
+    >
+      <p id="titulos" onClick={openContent}>
+        {titulo}
+      </p>
+      <div
+
+        style={{
+          maxHeight: divHeight,
+          overflow: "hidden",
+          transition: "max-height 0.05s linear",
+        }}
+        onTransitionEnd={handleTransitionEnd}
+      >
+        {body}
       </div>
-      <div hidden={viewHidden}>{body}</div>
     </section>
   )
 }
